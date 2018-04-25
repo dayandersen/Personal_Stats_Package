ee8ce36e508c110bc8e1290906ef91a51e83f856
ander612 <ander612@wwu.edu>
2018-04-24 23:57:14 -0700

    first commit with files

---
+++ hypothesis_tests.py
@@ -0,0 +1,10 @@
+# TODO List of tests I should add
+# 1. t-test
+# 2. f-test
+# 3. z-test
+# 4. Tukey-test
+# 5. ANOVA
+# 6. Pearson's chi-squared test
+# 7. G-test
+# 8. Pearson correlation coefficient
+# 9. 
